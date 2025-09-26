21db8a55a4f37c1cb3a0803a514408cef1279273
ncmalkyt900914 <malinkytta@gmail.com>
2025-09-26 18:19:30 +0200

    add 'Name' field to User class

--- User.cs
+++ User.cs
@@ -2,17 +2,19 @@ namespace App;
 
 public class User
 {
+    public string Name;
     public string Email;
     string _password;
 
-    public User(string email, string password)
+    public User(string name, string email, string password)
     {
+        Name = name;
         Email = email;
         _password = password;
     }
 
     public bool TryLogin(string email, string password)
     {
-        return Email == email && _password == password;
+        return email == Email && password == _password;
     }
 }
