dc1df992d1ab442fb18275e38572dae02f22b0ed
ncmalkyt900914 <malinkytta@gmail.com>
2025-10-01 15:12:58 +0200

    fix typo, make TradeStatus public and update Trade class
    Made TradeStatus enum public so Trade can access it
    and use TradeStatus type for status instead of string

--- Trade.cs
+++ Trade.cs
@@ -5,9 +5,9 @@ public class Trade
     public List<Item> Items;
     public User Sender;
     public User Receiver;
-    public string Status;
+    public TradeStatus Status;
 
-    public Trade(List<Item> items, User sender, User reciever, string status)
+    public Trade(List<Item> items, User sender, User reciever, TradeStatus status)
     {
         Items = items;
         Sender = sender;
